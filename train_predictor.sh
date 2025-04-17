444e906a24baa1b5cd14764338d7b1e0429eebd3
Your Name <you@example.com>
2025-04-17 10:48:46 +0000

    update

--- train_predictor.sh
+++ train_predictor.sh
@@ -1,4 +1,4 @@
-PYTHONPATH="./:${PYTHONPATH}" CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run --nproc_per_node=4 --master_port=1234 /fujiayi/IPC-Dehaze/basicsr/train.py -opt options/02train_predictor.yml --launcher pytorch 
+PYTHONPATH="./:${PYTHONPATH}" CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run --nproc_per_node=4 --master_port=1234 basicsr/train.py -opt options/02train_predictor.yml --launcher pytorch 
 
 
 
